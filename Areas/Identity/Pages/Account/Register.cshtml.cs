30b55cb08688e9594c8b0207fa917037bd765b08
alisondraV <alice.7414122013@gmail.com>
2021-10-14 20:17:03 -0400

    Update Login page

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,7 +46,6 @@ namespace ConestogaInsidersClub.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
             [Display(Name = "UserName")]
             public string UserName { get; set; }
@@ -90,8 +89,12 @@ namespace ConestogaInsidersClub.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.UserName, Email = Input.Email,
-                    FirstName = Input.FirstName, LastName = Input.LastName };
+                var user = new ApplicationUser {
+                    Email = Input.Email,
+                    UserName = Input.UserName,
+                    FirstName = Input.FirstName, 
+                    LastName = Input.LastName 
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
