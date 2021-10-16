a8685bdbfff65a68943c58053ae1951f45c2804b
alisondraV <alice.7414122013@gmail.com>
2021-10-16 13:11:27 -0400

    Add tests for authorized/unauthorized users access to pages

--- SeleniumTests/UserTests.cs
+++ SeleniumTests/UserTests.cs
@@ -3,41 +3,84 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.Generic;
 
 namespace SeleniumTests
 {
     public class Tests
     {
+        IWebDriver driver;
+        WebDriverWait wait;
+        private Dictionary<string, string> testUser = new Dictionary<string, string>();
+
         [SetUp]
         public void Setup()
         {
+            testUser.Add("UserName", "testUser");
+            testUser.Add("FirstName", "John");
+            testUser.Add("LastName", "Doe");
+            testUser.Add("Email", "j_doe@test.com");
+            testUser.Add("Password", "aA1234*");
+            testUser.Add("ConfirmPassword", "aA1234*");
+
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("https://localhost:5001");
+        }
+
+        [Test]
+        public void TestDoesNotShowPagesContentToUnauthorizedUsers()
+        {
+            driver.FindElement(By.LinkText("Profile")).Click();
+            wait.Until(webDriver => webDriver.FindElement(By.Id("Unauthorized_View")).Displayed);
         }
 
         [Test]
         public void TestUserCanRegisterAndLogIn()
         {
-            using (IWebDriver driver = new ChromeDriver())
+            driver.FindElement(By.LinkText("Register")).Click();
+
+            string testUserParamValue;
+            foreach(string key in testUser.Keys)
             {
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                driver.Navigate().GoToUrl("https://localhost:5001");
-                driver.FindElement(By.LinkText("Register")).Click();
-
-                driver.FindElement(By.Id("Input_UserName")).SendKeys("testUser");
-                driver.FindElement(By.Id("Input_FirstName")).SendKeys("John");
-                driver.FindElement(By.Id("Input_LastName")).SendKeys("Doe");
-                driver.FindElement(By.Id("Input_Email")).SendKeys("j_doe@test.com");
-                driver.FindElement(By.Id("Input_Password")).SendKeys("aA1234*");
-                driver.FindElement(By.Id("Input_ConfirmPassword")).SendKeys("aA1234*");
-                driver.FindElement(By.ClassName("btn-primary")).Click();
-
-                driver.FindElement(By.LinkText("Click here to confirm your account")).Click();
-                driver.FindElement(By.LinkText("Login")).Click();
-                driver.FindElement(By.Id("Input_UserName")).SendKeys("testUser");
-                driver.FindElement(By.Id("Input_Password")).SendKeys("aA1234*");
-                driver.FindElement(By.ClassName("btn-primary")).Click();
-
-                wait.Until(webDriver => webDriver.FindElement(By.LinkText("Hello, testUser!")).Displayed);
+                if (testUser.TryGetValue(key, out testUserParamValue))
+                {
+                    driver.FindElement(By.Id($"Input_{key}")).SendKeys(testUserParamValue);
+                }
             }
+            driver.FindElement(By.Id("Register_Button")).Click();
+
+            driver.FindElement(By.LinkText("Click here to confirm your account")).Click();
+            LogInAsATestUser();
+
+            wait.Until(webDriver => webDriver.FindElement(By.LinkText("Hello, testUser!")).Displayed);
+        }
+
+        [Test]
+        public void TestShowsPagesContentToAuthorizedUsers()
+        {
+            LogInAsATestUser();
+            wait.Until(webDriver => webDriver.FindElement(By.LinkText("Profile")).Displayed);
+            driver.FindElement(By.LinkText("Profile")).Click();
+
+            wait.Until(webDriver => webDriver.FindElement(By.Id("UserName")).Displayed);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+
+        private void LogInAsATestUser()
+        {
+            testUser.TryGetValue("UserName", out string testUserUserName);
+            testUser.TryGetValue("Password", out string testUserPassword);
+
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("Input_UserName")).SendKeys(testUserUserName);
+            driver.FindElement(By.Id("Input_Password")).SendKeys(testUserPassword);
+            driver.FindElement(By.Id("Log_In_Button")).Click();
         }
     }
 }
