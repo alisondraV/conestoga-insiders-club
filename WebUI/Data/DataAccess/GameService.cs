a9393061cc0bf3875a1a02539daf7f2213efdc37
Andrii Denysenko <andrey2850@gmail.com>
2021-10-22 11:32:09 -0400

    Add game service scaffold

---
+++ WebUI/Data/DataAccess/GameService.cs
@@ -0,0 +1,36 @@
+﻿using ConestogaInsidersClub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConestogaInsidersClub.Data.DataAccess
+{
+    public class GameService : IGameService
+    {
+        public Task deleteGame(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Game> getGame(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Game>> getGames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Game>> searchGames(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Game> updateGame(int gameId, Game newGame)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebUI/Data/DataAccess/IGameService.cs
@@ -0,0 +1,17 @@
+﻿using ConestogaInsidersClub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConestogaInsidersClub.Data.DataAccess
+{
+    interface IGameService
+    {
+        Task<List<Game>> getGames();
+        Task<Game> getGame(int gameId);
+        Task<List<Game>> searchGames(string name);
+        Task<Game> updateGame(int gameId, Game newGame);
+        Task deleteGame(int gameId);
+    }
+}
