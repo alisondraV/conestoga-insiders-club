8e94ddccdf9276b108385e4bcb43be33f00d0dea
Andrii Denysenko <andrey2850@gmail.com>
2021-10-27 03:18:19 -0400

    Set up in memory tests

---
+++ UnitTests/GameServiceTests.cs
@@ -0,0 +1,85 @@
+using ConestogaInsidersClub.Data;
+using ConestogaInsidersClub.Data.DataAccess;
+using ConestogaInsidersClub.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ServiceTests
+{
+    public class GameServiceTests : TestBase
+    {
+        public GameGenre genre;
+        public List<Game> expectedGames;
+
+        [OneTimeSetUp]
+        public async Task BeforeAll()
+        {
+            using var context = new ApplicationDbContext(ContextOptions);
+
+            genre = new GameGenre()
+            {
+                Name = "Horror",
+            };
+            await context.Set<GameGenre>().AddAsync(genre);
+
+            expectedGames = new List<Game> {
+                new Game()
+                {
+                    GameId = 1,
+                    Name = "Foo bar",
+                    Description = "Baz Fizz"
+                },
+                new Game()
+                {
+                    GameId = 2,
+                    Name = "Other bar",
+                    Description = "Other Fizz"
+                }
+            };
+
+            foreach (var game in expectedGames)
+            {
+                game.Genre = genre.Name;
+                await context.Set<Game>().AddAsync(game);
+            }
+
+            await context.SaveChangesAsync();
+        }
+
+        [Test]
+        public async Task GetGames_ShouldListAllGames()
+        {
+            // Arrange
+            using var context = new ApplicationDbContext(ContextOptions);
+            var service = new GameService(context);
+
+            // Act
+            var actualGames = await service.GetGames();
+
+            // Assert
+            Assert.NotNull(actualGames);
+            Assert.That(actualGames, Has.Count.EqualTo(expectedGames.Count));
+        }
+
+        [Test]
+        public async Task GetGame_ShouldGetAGameById()
+        {
+            // Arrange
+            using var context = new ApplicationDbContext(ContextOptions);
+            var service = new GameService(context);
+            var expectedGame = expectedGames[0];
+            var otherGame = expectedGames[1];
+
+            // Act
+            var actualGame = await service.GetGame(expectedGame.GameId);
+
+            // Assert
+            Assert.NotNull(actualGame);
+            Assert.AreEqual(actualGame.Name, expectedGame.Name);
+            Assert.AreNotEqual(actualGame.Name, otherGame.Name);
+        }
+    }
+}
---
+++ UnitTests/TestBase.cs
@@ -0,0 +1,31 @@
+﻿using ConestogaInsidersClub.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTests
+{
+    public class TestBase
+    {
+        protected DbContextOptions<ApplicationDbContext> ContextOptions { get; }
+
+        public TestBase()
+        {
+            ContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "testing").Options;
+            Seed();
+        }
+
+        private void Seed()
+        {
+            using var context = new ApplicationDbContext(ContextOptions);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            context.SaveChanges();
+        }
+    }
+}
