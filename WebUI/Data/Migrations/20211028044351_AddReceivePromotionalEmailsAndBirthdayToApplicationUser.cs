6a0b37cf865b2f5950239fd715bb52d68ba51d43
Andrii Denysenko <andrey2850@gmail.com>
2021-11-02 19:11:10 -0400

    Add promoemails and birthday columns to User (#8)

---
+++ WebUI/Data/Migrations/20211028044351_AddReceivePromotionalEmailsAndBirthdayToApplicationUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConestogaInsidersClub.Data.Migrations
+{
+    public partial class AddReceivePromotionalEmailsAndBirthdayToApplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "birthday",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "receive_promotional_emails",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "birthday",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "receive_promotional_emails",
+                table: "AspNetUsers");
+        }
+    }
+}
