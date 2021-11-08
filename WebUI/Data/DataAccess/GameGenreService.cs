4cee55e7add68ad7840260d123f3b02d33c78b91
Andrii Denysenko <andrey2850@gmail.com>
2021-11-07 22:41:14 -0500

    Add GameGenre Service

---
+++ WebUI/Data/DataAccess/GameGenreService.cs
@@ -0,0 +1,24 @@
+﻿using ConestogaInsidersClub.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConestogaInsidersClub.Data.DataAccess
+{
+    public class GameGenreService : IGameGenreService
+    {
+        private readonly ApplicationDbContext context;
+
+        public GameGenreService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<List<GameGenre>> GetGameGenres()
+        {
+            return context.GameGenres.ToListAsync();
+        }
+    }
+}
---
+++ WebUI/Data/DataAccess/IGameGenreService.cs
@@ -0,0 +1,13 @@
+﻿using ConestogaInsidersClub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConestogaInsidersClub.Data.DataAccess
+{
+    interface IGameGenreService
+    {
+        Task<List<GameGenre>> GetGameGenres();
+    }
+}
