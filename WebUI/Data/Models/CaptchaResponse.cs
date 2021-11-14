b079496116fe471d6d45f24d581be1248232a400
MorganDWalker <49662330+MorganDWalker@users.noreply.github.com>
2021-11-14 14:57:33 -0500

    Captcha (#35)

---
+++ WebUI/Data/Models/CaptchaResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ConestogaInsidersClub.Areas.Identity.Pages.Account
+{
+    public class CaptchaResponse
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+        [JsonProperty("error-codes")]
+        public List<string> ErrorMessage { get; set; }
+    }
+}
