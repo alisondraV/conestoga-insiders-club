676f177f4d242cec73305d954a28b4118b8b1c42
Morgan Walker <GreenPakman@gmail.com>
2021-11-22 10:14:49 -0500

    Laying down framework for Events CRUD

---
+++ WebUI/Data/DataAccess/EventService.cs
@@ -0,0 +1,41 @@
+﻿using ConestogaInsidersClub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConestogaInsidersClub.Data.DataAccess
+{
+    public class EventService : IEventService
+    {
+        public Task AddEvent(Event @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEvent(Event @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> GetEvent(int eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Event>> GetEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task JoinEvent(Event @event, ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEvent(Event @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebUI/Data/DataAccess/IEventService.cs
@@ -0,0 +1,19 @@
+﻿using ConestogaInsidersClub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConestogaInsidersClub.Data.DataAccess
+{
+    interface IEventService
+    {
+        Task AddEvent(Event @event);
+        Task DeleteEvent(Event @event);
+        Task UpdateEvent(Event @event);
+        Task<Event> GetEvent(int eventId);
+        Task<List<Event>> GetEvents();
+        Task JoinEvent(Event @event, ApplicationUser user);
+
+    }
+}
