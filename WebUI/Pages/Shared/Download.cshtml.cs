8bcf28cf7e8764aff182fc4e6fd0d58f0a15b910
Andrii Denysenko <andrey2850@gmail.com>
2021-11-24 12:50:43 -0500

    Make downloads safer

--- WebUI/Pages/Shared/Download.cshtml.cs
+++ WebUI/Pages/Shared/Download.cshtml.cs
@@ -1,4 +1,5 @@
 using ConestogaInsidersClub.Data.DataAccess;
+using ConestogaInsidersClub.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
@@ -10,25 +11,42 @@ namespace ConestogaInsidersClub.Pages.Shared
     public class DownloadModel : PageModel
     {
         IGameService gameService;
+        IUserService userService;
 
-        public DownloadModel(IGameService gameService)
+        public DownloadModel(IGameService gameService, IUserService userService)
         {
             this.gameService = gameService;
+            this.userService=userService;
         }
 
         public async Task<IActionResult> OnGet(string gameId)
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Unauthorized();
+            }
+
             try
             {
                 var game = await gameService.GetGame(int.Parse(gameId));
-                var content = $"{game.Name}\n{game.Description}";
-                var bytes = Encoding.ASCII.GetBytes(content);
-                return File(bytes, "application/octet-stream", $"{game.Name}.txt");
+                var user = await userService.GetUser(User.Identity.Name);
+                if (!gameService.IsOwnedBy(game, user.Id))
+                {
+                    return Unauthorized();
+                }
+                return CreateFile(game);
             }
             catch (Exception)
             {
                 return NotFound();
             }
+        }
+
+        private IActionResult CreateFile(Game game)
+        {
+            var content = $"{game.Name}\n{game.Description}";
+            var bytes = Encoding.ASCII.GetBytes(content);
+            return File(bytes, "application/octet-stream", $"{game.Name}.txt");
         }
     }
 }
