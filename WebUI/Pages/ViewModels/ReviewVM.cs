ca127729cc1456840ecda04a7357d1117a83f3d5
Steven Tripp <50451074+SJETInc@users.noreply.github.com>
2021-11-25 16:35:51 -0500

    Draft of user game review.

---
+++ WebUI/Pages/ViewModels/ReviewVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using ConestogaInsidersClub.Data;
+using ConestogaInsidersClub.Data.Models;
+
+namespace ConestogaInsidersClub.Pages.ViewModels
+{
+    public class ReviewVM
+    {
+        public ReviewVM(Review review)
+        {
+            Rating = review.Rating;
+            Description = review.Description;
+            Approved = review.Approved;
+        }
+
+
+        [Required(ErrorMessage = "Rating is required.")]
+        public byte Rating { get; set; }
+
+        [Required(ErrorMessage = "Comment required.")]
+        public string Description { get; set; }
+
+        public bool? Approved { get; set; }
+
+        public Review ToModel(Review review)
+        {
+            review.Rating = Rating;
+            review.Description = Description;
+            review.Approved = Approved;
+
+            return review;
+        }
+    }
+}
